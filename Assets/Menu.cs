90c9abe7f63f370d9ed182c2a7d3ec96098f9ef9
notHi3u <buihoanghieu21122003@gmail.com>
2024-05-12 13:37:26 +0700

    Menu,gameover(bugged)

---
+++ Assets/Menu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+
+    }
+}
